2e2dc6f27d0ee9c463227f6bcb350a98742ef6ab
Paula Fenner <fenner.paula@gmail.com>
2025-02-05 15:26:21 +0100

    First commit - base of project.

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import About from './components/About.jsx';
+import Home from './components/Home.jsx';
+import Vans from './components/Vans.jsx';
+import VanDetail from './components/VanDetail';
+
+import './server.js';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
+        <Route path="/vans/:id" element={<VanDetail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
