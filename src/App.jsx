c9ec9aee94db085cc8a31df954ebea4bf911258f
Paula Fenner <fenner.paula@gmail.com>
2025-02-07 17:29:44 +0100

    Added Components and Host nav bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import About from './components/About.jsx';
-import Home from './components/Home.jsx';
-import Vans from './components/Vans.jsx';
-import VanDetail from './components/VanDetail';
+import Layout from './components/Layout.jsx';
+
+import About from './pages/About.jsx';
+import Home from './pages/Home.jsx';
+import Vans from './pages/Vans/Vans.jsx';
+import VanDetail from './pages/Vans/VanDetail.jsx';
+
+import Dashboard from './pages/Host /Dashboard.jsx';
+import Income from './pages/Host /Income.jsx';
+import Reviews from './pages/Host /Reviews.jsx';
 
 import './server.js';
+import HostLayout from './components/HostLayout.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VanLife
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+
+          <Route path="/host" element={<HostLayout />}>
+            <Route path="/host" element={<Dashboard />} />
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
+
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
