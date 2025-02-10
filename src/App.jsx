cef1dd4f8ed1e599789d448960c3941300c53353
Paula Fenner <fenner.paula@gmail.com>
2025-02-10 15:47:14 +0100

    Added Footer and styling to active elements on the navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,25 @@ import Dashboard from './pages/Host /Dashboard.jsx';
 import Income from './pages/Host /Income.jsx';
 import Reviews from './pages/Host /Reviews.jsx';
 
-import './server.js';
 import HostLayout from './components/HostLayout.jsx';
+
+import './server.js';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
 
-          <Route path="/host" element={<HostLayout />}>
-            <Route path="/host" element={<Dashboard />} />
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
-
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
         </Route>
       </Routes>
     </BrowserRouter>
